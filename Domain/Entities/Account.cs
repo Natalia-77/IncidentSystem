20c3c92d8d9ceaae146b3199e78f0c9d5c152fb1
vlasnata80@gmail.com <vlasnata80@gmail.com>
2022-06-02 22:31:30 +0300

    Create entities

---
+++ Domain/Entities/Account.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Domain.Entities
+{
+    public class Account
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public ICollection<Contact> Contacts { get; set; }
+        public int IncidentId { get; set; }
+        public virtual Incident Incident { get; set; }
+    }
+}
