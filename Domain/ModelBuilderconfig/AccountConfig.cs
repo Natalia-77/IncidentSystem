7c2389068cab12264542aa82047615cb0aadaa9a
vlasnata80@gmail.com <vlasnata80@gmail.com>
2022-06-06 11:19:27 +0300

    Add entities config

---
+++ Domain/ModelBuilderconfig/AccountConfig.cs
@@ -0,0 +1,26 @@
+﻿using Domain.Entities;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Domain.ModelBuilderconfig
+{
+    public class AccountConfig : IEntityTypeConfiguration<Account>
+    {
+        public void Configure(EntityTypeBuilder<Account> builder)
+        {
+            builder.ToTable("tblAccount");
+            builder.HasKey(key => key.Id);
+            builder.Property(p => p.Id).HasColumnName("Id");
+            builder.Property(p => p.Name)
+                .HasColumnName("AccountName")
+                .HasMaxLength(150)
+                .IsRequired();
+
+        }
+    }
+}
---
+++ Domain/ModelBuilderconfig/ContactConfig.cs
@@ -0,0 +1,35 @@
+﻿using Domain.Entities;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Domain.ModelBuilderconfig
+{
+    public class ContactConfig : IEntityTypeConfiguration<Contact>
+    {
+        public void Configure(EntityTypeBuilder<Contact> builder)
+        {
+            builder.ToTable("tblContact");
+            builder.HasKey(key => key.Id);
+            builder.Property(p => p.Id).HasColumnName("Id");
+            builder.Property(p => p.FirstName)
+                .HasColumnName("FirstName")
+                .HasMaxLength(200)
+                .IsRequired();
+            builder.Property(p => p.LastName)
+                .HasColumnName("LastName")
+                .HasMaxLength(200)
+                .IsRequired();
+            builder.Property(p => p.Email)
+                .HasColumnName("Email")
+                .IsRequired();
+            builder.HasOne(a => a.Account)
+                .WithMany(c => c.Contacts)
+                .HasForeignKey(fk => fk.AccountId);
+        }
+    }
+}
